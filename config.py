b4f2a011dd3157bd68cca2298662d4109e1ae781
Klondike <147417684+MyKlondike@users.noreply.github.com>
2023-11-04 13:35:18 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Сколько ETH бриджить в Scroll:
+
+amount_from = 0.01
+amount_to = 0.02
+
+# MAX_GWEI в ETH и Base:
+
+MAX_GWEI = 40
+Base_GWEI = 0.2
+
+# Задержка между кошельками:
+
+delay_from = 1
+delay_to = 1000
